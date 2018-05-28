bc7eb06f6689a0527650f18b20212472a4880366
Vegardtve <37774253+Vegardtve@users.noreply.github.com>
2018-05-28 13:48:37 +0200

    Delete surfzlim.m

--- surfzlim.m
+++
@@ -1,6 +0,0 @@
-function surfzlim(source,event)
-        val = 51 - source.Value;
-        % For R2014a and earlier:
-        % val = 51 - get(source,'Value')
-        set(source,'Tag','val');
-    end
