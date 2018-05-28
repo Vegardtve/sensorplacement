a18879cd03c34b1ac90739242ae2abc633818aa6
Vegardtve <37774253+Vegardtve@users.noreply.github.com>
2018-05-28 13:48:45 +0200

    Delete test.m

--- test.m
+++
@@ -1,5 +0,0 @@
-f = figure;
-f.MenuBar = 'none';    % Hide standard menu bar menus.
-mh = uimenu(f,'Label','My menu');
-eh1 = uimenu(mh,'Label','Item 1');
-eh2 = uimenu(mh,'Label','Item 2','Checked','on');
